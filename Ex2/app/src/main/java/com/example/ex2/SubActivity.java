42f76bf2da544d97194b3edd3185d16d29ab70db
duong <duonghien491@gmail.com>
2024-09-29 00:16:16 +0700

    Ex2_CREATE_SUBACTIVITY

---
+++ Ex2/app/src/main/java/com/example/ex2/SubActivity.java
@@ -0,0 +1,33 @@
+package com.example.ex2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SubActivity extends AppCompatActivity {
+Button btnok;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sub);
+        btnok = findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+            });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
